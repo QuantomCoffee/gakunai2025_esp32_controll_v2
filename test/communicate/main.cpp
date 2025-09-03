d2f0b1f79137f2511c508bce34782961dd4e8a76
Quantom Coffee (Alternative) <e1201025049@oit.ac.jp>
2025-09-03 22:13:08 +0900

    v0.4.000 init
    Githubでの開発へ移行
    CLIでどうもうまくいかないのでGithub Desktop使いました
    動きやすいし...

---
+++ test/communicate/main.cpp
@@ -0,0 +1,188 @@
+#include <Arduino.h>
+#include <PS4Controller.h>
+#include <SMS_STS.h>
+
+#define S2_TX 16
+#define S2_RX 17
+#define S0_TX 0
+#define S0_RX 1
+
+#define AUTO_SWITCH_MS 7500 // 起動後 何ms で自動に切り替えるか
+
+// define global status
+
+bool automationEnable = false; // falseの場合は自動化しない (ラズパイは虚空に送り続ける)
+bool arm_status; // これは後で位置を確認するので未定義。
+SMS_STS Servo;
+int prev_ms;
+int sw_time;
+int delta_sw_time;
+
+// put function declarations here:
+int myFunction(int, int);
+int8_t culc_checksum(int8_t*);
+bool test_checksum(int8_t*);
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200); // to PC, or RaspberryPi
+  Serial2.begin(115200,134217756U,S2_RX,S2_TX); // to ESP32
+  PS4.begin("90:15:06:7c:3e:26"); // PS4 Controller
+
+  // for FEETECH Servo (STS3215 12V)
+  // Servo.Ping(0); //アームは1～6を使用、ベルトコンベアは知らん。
+  prev_ms = millis();
+  sw_time = millis();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  int8_t movement[8] = {0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0};
+  // 行動パターンを決めます。1バイト目は0x58で固定です。8バイト目はチェックサムです。
+  // movementは基本的にそのままESP32(下)に転送します。
+
+  // int8_t senddata[8] = {0x58,0xfe,0x0,0x0,0x0,0x0,0x0,0x0};
+  // ラズパイ/PCに返却するデータです。
+  // 余裕がないので消えました。
+
+
+  // コントローラーはいきてますかー
+  if (PS4.isConnected())
+  {
+    // 死活タイマーリセット
+    sw_time = millis();
+    delta_sw_time = sw_time + 0;
+    // senddata[6]=0x10;
+
+    // 動きを指定(というか直接動かす)
+
+    // MODE 1 | 運転メイン用 キーコンフィグ
+    #define GUAGE_DRIVE_ACCEL PS4.R2Value() //加速度を決定します。L2またはR2またはスティックが設定できます。
+    #define STICK_DRIVE_FRONT PS4.LStickY() //平行移動時、前に進む勢いを示します。スティックが設定できます。
+    #define STICK_DRIVE_RIGHT PS4.LStickX() //平行移動時、右に進む勢いを示します。スティックが設定できます。
+    #define KEY_ROTATE_CW PS4.Right() //右(上から見て時計回り)へ回転移動します。ボタンが設定できます。
+    #define KEY_ROTATE_CCW PS4.Left() //左(上から見て反時計回り)へ回転移動します。ボタンが設定できます。
+    #define KEY_DRIVE_FRONT PS4.Up() //前方へ進む。ボタンが設定できます。
+    #define KEY_DRIVE_BACK PS4.Down() //後方へ進む。ボタンが設定できます。
+
+    // MODE 2 | アームメイン用 キーコンフィグ
+    #define KEY_ARM_HOLD PS4.Circle() //アームでつかむ・はなすを切り替えます。はなす場合は後述の解放キーを同時に押す必要があります。
+    #define KEY_ARM_REL_LOCK PS4.R1() //アームで話す場合の安全ロックです。ボタンまたはTrueが設定できます。Trueを代入すると無効になります。
+    #define STICK_ARM_FRONT PS4.LStickY() //アーム先端を操縦時、前に進む勢いを示します。スティックが設定できます。
+    #define STICK_ARM_RIGHT PS4.LStickX() //アーム先端を操縦時、右に進む勢いを示します。スティックが設定できます。
+    #define KEY_ARM_UP PS4.Up() //アームの根元を前へ進めたり、アーム先端を上げたりします。
+    #define KEY_ARM_DW PS4.Uown() //アームの根元を後へ進めたり、アーム先端を下げたりします。
+    #define KEY_ARM_SW PS4.Share() //押されてる間、アームの根元を移動させます。ボタンが設定できます。
+
+    // MODE 2+| 予備
+    #define KEY_RAIL_ON PS4.Square() //レールを起動します
+    #define KEY_RAIL_OFF PS4.Triangle() //レールをオフにします
+
+    #define KEY_MODE_SW PS4.Options() //単押しで運転モードとアームモードを切り替えます。長押しすると自動化を起動します。
+
+    static int mode_ = 1;
+
+    if(mode_==2){
+      // やだああああああああああああああああああああ
+    }else{
+      if (GUAGE_DRIVE_ACCEL)
+      {
+        if(KEY_ROTATE_CCW&&KEY_ROTATE_CW){
+          Serial.println("WHAT?");
+        }else if(KEY_DRIVE_BACK&&KEY_DRIVE_FRONT){
+          Serial.println("WHAT??");
+        }else if((KEY_ROTATE_CCW||KEY_ROTATE_CW)&&(KEY_DRIVE_BACK||KEY_DRIVE_FRONT)){
+          Serial.println("WHAT???");
+        }else if(KEY_ROTATE_CW){
+          movement[1]=0x21;
+          movement[5]=(GUAGE_DRIVE_ACCEL>>1)&0x7f;
+        }else if(KEY_ROTATE_CCW){
+          movement[1]=0x21;
+          movement[5]=(GUAGE_DRIVE_ACCEL>>1)&0x7f;
+        }else if(KEY_DRIVE_FRONT){
+          movement[1]=0x20;
+          movement[2]=0x7f;
+          movement[3]=0x00;
+          movement[4]=(GUAGE_DRIVE_ACCEL>>1)&0x7f;
+        }else if(KEY_DRIVE_BACK){
+          movement[1]=0x20;
+          movement[2]=-0x7f;
+          movement[3]=0x00;
+          movement[4]=(GUAGE_DRIVE_ACCEL>>1)&0x7f;
+        }else if((STICK_DRIVE_FRONT>=16)||(STICK_DRIVE_RIGHT>=16)){
+          movement[1]=0x20;
+          movement[2]=STICK_DRIVE_FRONT&0xf0;
+          movement[3]=STICK_DRIVE_RIGHT&0xf0;
+          movement[4]=(GUAGE_DRIVE_ACCEL>>1)&0x7f;
+        }else{
+          movement[1]=0x01;
+        }
+      }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+  /*
+  if (Serial.available()&&automationEnable&&movement[0]==0x0) //何も書き込まれてなくて、自動化がOKなら。
+  {
+    delta_sw_time = millis() - sw_time; //さすがにノータイムで自動運転始めるとまずいので...
+    if (delta_sw_time >= AUTO_SWITCH_MS) {
+      // senddata[6]=0x40;
+      int8_t temp_mov[8];
+      if(Serial.readBytes(temp_mov,8);==8){
+        if(test_checksum(temp_mov)){
+          for (size_t i = 0; i < 8; i++) {movement[i]=temp_mov[i];}        
+        }
+      }
+    } else {
+      // senddata[6]=0x60;
+    }
+  } */
+  
+
+  
+
+
+
+
+  
+
+
+  // CheckSum
+  // senddata[7]=culc_checksum(senddata);
+  // Serial.write(senddata,8);
+
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
+
+int8_t culc_checksum(int8_t* data){
+  uint16_t tempval = 0;
+  for (size_t i = 0; i < 7; i++){
+    tempval+=data[i];
+  }
+  return (tempval&0xff);  
+}
+
+bool test_checksum(int8_t* data){
+  return (data[7]==culc_checksum(data));
+};
