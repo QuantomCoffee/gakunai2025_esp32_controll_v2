2702ac56bf025f8297cba2b9ad53f4e7d5e369b0
Quantom Coffee (Alternative) <e1201025049@oit.ac.jp>
2025-09-17 03:35:17 +0900

    v0.4.006 これでロボットが動いてほしい。まじで。

--- test/communicate/main.cpp
+++ test/communicate/main.cpp
@@ -3,15 +3,24 @@
 #include <SMS_STS.h> // STServo.zipより
 #include <Adafruit_TSL2591.h> //カラーセンサー
 
-#define S2_TX 16
-#define S2_RX 17
-#define S0_TX 0
-#define S0_RX 1
+// pin群 
+#define S2_TX 16 // to ESP(lowerhalf)
+#define S2_RX 17 // to ESP(lowerhalf)
+#define S1_TX 18 // to FEETECH Servo
+#define S1_RX 19 // to FEETECH Servo
+#define S0_TX 1 // to PC or RaspberryPi
+#define S0_RX 3 // to PC or RaspberryPi
 
+// そのほか設定 
 #define AUTO_SWITCH_MS 7500 // 起動後 何ms で自動に切り替えるか
 
-// define global status
+// アーム用のあれこれ 
+#define LEG_2 0.0 // サーボ2<->3 の長さ (mm)
+#define LEG_3 0.0 // サーボ3<->4 の長さ (mm)
+#define LEG_4 0.0 // サーボ4<->5 の長さ (mm)
+#define LEG_5 0.0 // サーボ5<->6 の長さ (mm)
 
+// define global status
 bool automationEnable = false; // falseの場合は自動化しない (ラズパイは虚空に送り続ける)
 bool arm_status; // これは後で位置を確認するので未定義。
 SMS_STS Servo;
@@ -25,14 +34,21 @@ int8_t culc_checksum(int8_t*);
 bool test_checksum(int8_t*);
 
 
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200); // to PC, or RaspberryPi
+  Serial1.begin(1000000,134217756U,S1_RX,S1_TX);
   Serial2.begin(115200,134217756U,S2_RX,S2_TX); // to ESP32
   PS4.begin("90:15:06:7c:3e:26"); // PS4 Controller
 
   // for FEETECH Servo (STS3215 12V)
-  // Servo.Ping(0); //アームは1～6を使用、ベルトコンベアは知らん。
+  Servo.pSerial=&Serial1;
+  Servo.WritePosEx(1,0,0);
+  Servo.WritePosEx(2,0,0);
+
+  // Servo.Ping(1); //アームは1～6を使用、ベルトコンベアは知らん。
   prev_ms = millis();
   sw_time = millis();
 }
@@ -72,7 +88,7 @@ void loop() {
     #define KEY_ARM_HOLD PS4.Circle() //アームでつかむ・はなすを切り替えます。はなす場合は後述の解放キーを同時に押す必要があります。
     #define KEY_ARM_REL_LOCK PS4.R1() //アームで話す場合の安全ロックです。ボタンまたはTrueが設定できます。Trueを代入すると無効になります。
     #define STICK_ARM_FRONT PS4.LStickY() //アーム先端を操縦時、前に進む勢いを示します。スティックが設定できます。
-    #define STICK_ARM_RIGHT PS4.LStickX() //アーム先端を操縦時、右に進む勢いを示します。スティックが設定できます。
+    #define STICK_ROTATE PS4.LStickX() //機体を回転させます。スティックが設定できます。
     #define KEY_ARM_UP PS4.Up() //アームの根元を前へ進めたり、アーム先端を上げたりします。
     #define KEY_ARM_DW PS4.Down() //アームの根元を後へ進めたり、アーム先端を下げたりします。
     #define KEY_ARM_SW PS4.Share() //押されてる間、アームの根元を移動させます。ボタンが設定できます。
@@ -108,77 +124,68 @@ void loop() {
     
     if(mode_==2){
       // やだああああああああああああああああああああ
+      PS4.setLed(0x30,0xff,0x30);
+      if(abs(STICK_ROTATE-128)>=32){
+        movement[1]=0x21;
+        movement[4]=STICK_ROTATE; //
+      }
+
+      // put アームの処理 here.
+
+      // 現在地取得
+
+      // 差分計算
+
+      // 動かす
+
     }else{
       PS4.setLed(0xff,0xb7,0x00);
       // 加速する。優先度は回転>微調整>スティック
-        // movement[1]には操作の種類、[2],[3]はX,Y成分、[4]には速度(場合によっては時計回りのみ)が入力される
-      if(KEY_ROTATE_CCW&&KEY_ROTATE_CW){
-        // 左右回転が同時に押されたとき
+      // movement[1]には操作の種類、[2],[3]はX,Y成分、[4]には速度(場合によっては時計回りのみ)が入力される
+      if(KEY_ROTATE_CCW&&KEY_ROTATE_CW){ // 左右回転が同時に押されたとき
         Serial.println("WHAT?");
-      }else if(KEY_DRIVE_BACK&&KEY_DRIVE_FRONT){
-        // 前後移動が同時に押されたとき
+      }else if(KEY_DRIVE_BACK&&KEY_DRIVE_FRONT){ // 前後移動が同時に押されたとき
         Serial.println("WHAT??");
-      }else if((KEY_ROTATE_CCW||KEY_ROTATE_CW)&&(KEY_DRIVE_BACK||KEY_DRIVE_FRONT)){
-        // 回転移動指定と前後移動指定が同時に押されたとき
+      }else if((KEY_ROTATE_CCW||KEY_ROTATE_CW)&&(KEY_DRIVE_BACK||KEY_DRIVE_FRONT)){ // 回転移動指定と前後移動指定が同時に押されたとき
         Serial.println("WHAT???");
-      }else if(KEY_ROTATE_CW){
-        // 時計回りに回転
+      }else if(KEY_ROTATE_CW){ // 時計回りに回転
         movement[1]=0x21;
         movement[4]=(GUAGE_DRIVE_ACCEL>>1);
-      }else if(KEY_ROTATE_CCW){
-        // 反時計回りに回転
+      }else if(KEY_ROTATE_CCW){ // 反時計回りに回転
         movement[1]=0x21;
         movement[4]=(0x100-(GUAGE_DRIVE_ACCEL>>1))&0xff; // 0xff(=-1)
-      }else if(KEY_DRIVE_FRONT){
-        // 前方へ全力前進
+      }else if(KEY_DRIVE_FRONT){ // 前方へ全力前進
         movement[1]=0x20;
         movement[2]=0x7f;
         movement[3]=0x00;
         movement[4]=(GUAGE_DRIVE_ACCEL>>1);
-      }else if(KEY_DRIVE_BACK){
-        // 後方へ全力前進
+      }else if(KEY_DRIVE_BACK){ // 後方へ全力前進
         movement[1]=0x20;
         movement[2]=0x81; //-127
         movement[3]=0x00;
         movement[4]=(GUAGE_DRIVE_ACCEL>>1);
       }else if (GUAGE_DRIVE_ACCEL) { //方向指定がなければ
-        if((STICK_DRIVE_FRONT>=16)||(STICK_DRIVE_RIGHT>=16)){
-          // とりあえず動かす
+        if((STICK_DRIVE_FRONT>=16)||(STICK_DRIVE_RIGHT>=16)){ // とりあえず動かす
           movement[1]=0x20;
           movement[2]=STICK_DRIVE_FRONT&0xf8;
           movement[3]=STICK_DRIVE_RIGHT&0xf8;
           movement[4]=(GUAGE_DRIVE_ACCEL>>1);
         }
-      }else {
-        // なんもないなら止めようぜ
+      }else { // なんもないなら止めようぜ
         movement[1]=0x2f;
       }
     }
 
-    // PS4にカラーセンサーの色などを反映
+    // PS4に状態カラーセンサーの色などを反映
     PS4.sendToController();
     
     // CheckSum
     movement[7]=culc_checksum(movement);
+
+    // ESPにぶん投げる
     Serial2.write(movement,8);
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  }
+  
+}
   /*
   if (Serial.available()&&automationEnable&&movement[0]==0x0) //何も書き込まれてなくて、自動化がOKなら。
   {
